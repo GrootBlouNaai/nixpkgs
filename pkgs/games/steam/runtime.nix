317f1dfe4d36f7ecc5dedc708c86b44ed5318231
RoboPatcher24 <108548496+GrootBlouNaai@users.noreply.github.com>
2024-09-23 16:50:54 +0200

    Explain

--- pkgs/games/steam/runtime.nix
+++ pkgs/games/steam/runtime.nix
@@ -1,3 +1,6 @@
+# Steam runtime package
+# This package includes the official runtime environment used by Steam.
+# It is fetched from the Steam repository and includes various libraries and binaries required by Steam.
 { lib, stdenv, fetchurl
 
 # for update script
@@ -6,28 +9,37 @@
 
 stdenv.mkDerivation (finalAttrs: {
 
+  # Package name
   pname = "steam-runtime";
-  # from https://repo.steampowered.com/steamrt-images-scout/snapshots/latest-steam-client-general-availability/VERSION.txt
+
+  # Version of the Steam runtime
+  # This version is fetched from the Steam repository and should be updated periodically.
   version = "0.20240415.84615";
 
+  # Source URL and hash for the Steam runtime tarball
   src = fetchurl {
     url = "https://repo.steampowered.com/steamrt-images-scout/snapshots/${finalAttrs.version}/steam-runtime.tar.xz";
     hash = "sha256-C8foNnIVA+O4YwuCrIf9N6Lr/GlApPVgZsYgi+3OZUE=";
     name = "scout-runtime-${finalAttrs.version}.tar.gz";
   };
 
+  # Build command to extract the runtime tarball into the output directory
   buildCommand = ''
     mkdir -p $out
     tar -C $out --strip=1 -x -f $src
   '';
 
+  # Passthru attributes for additional functionality
   passthru = {
+    # Update script to fetch the latest version of the Steam runtime
+    # This script uses `curl` to fetch the latest version number and `nix-update` to update the package.
     updateScript = writeShellScript "update.sh" ''
       version=$(${curl}/bin/curl https://repo.steampowered.com/steamrt-images-scout/snapshots/latest-steam-client-general-availability/VERSION.txt)
       ${lib.getExe nix-update} --version "$version" steamPackages.steam-runtime
     '';
   };
 
+  # Metadata for the package
   meta = {
     description = "Official runtime used by Steam";
     homepage = "https://github.com/ValveSoftware/steam-runtime";
